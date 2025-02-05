9a05dae9c50eecc018d7d961e676ca06857ccc83
YeoleKrushna <yeolekrushna00@gmail.com>
2025-02-05 12:01:29 +0530

    Day1

---
+++ day1.py
@@ -0,0 +1,69 @@
+# print("Hello World")
+
+# p = "Hello"
+# print("e" not in p)
+
+# a = 2
+# b ='2'
+# print(a is b)
+
+# value = (1+1)*2**4//3+4-1
+# print(value)
+
+
+# # b = 1000 
+# # while b > 0:
+# #     a = int(input(f"Enter the amount to withdraw (Available balance: {b}): "))
+# #     if a <= b:
+# #         b -= a 
+# #         print(f"You've withdrawn {a}. Your remaining balance is {b}.")
+# #     else:
+# #         print("Insufficient balance. Try again.")
+# #         break
+
+# i =1
+# while i<=3:
+#     print("Outer",i)
+#     i+=1
+#     j =1
+#     while j<=5:
+#         print("Inner",j)
+#         j+=1
+# print("he")
+
+# #range
+# c=range(5,0,-1)
+# print(c[0])
+# print(c[1])
+# print(c[2])
+# print(c[3])
+# print(c[4])
+
+
+
+# st="HEY"
+# y = len(st)
+# print(y)
+# ch = 0
+# while ch < y:
+#     print(ch, "=", st[ch])
+#     ch += 1
+
+# st = "GeekyShows"
+# for ch in st:
+#     print(ch)
+# else:
+#     print("Else exe")
+# print("Rest")
+
+# for i in range(3):
+#     print("Outr",i)
+#     for j in range(5):
+#         print("inner =",j)
+# print("Rest")
+
+for i in range(10):
+    if(i==5):
+        continue
+    print(i)
+print("rest")
